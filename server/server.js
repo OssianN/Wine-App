941e47883be2c73c8720e8f839c1699bc878e4e3
OssianN <ossian.northen@gmail.com>
2020-11-21 19:51:43 +0100

    security updates with mongo

--- server/server.js
+++ server/server.js
@@ -6,9 +6,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const dbName = 'WineDB';
-const pswd = 'S7uEAKEI3gQaut7g';
-console.log(process.env.PASSWORD)
-const uri = `mongodb://ossian:${pswd}@winedb-shard-00-00.a5qld.mongodb.net:27017,winedb-shard-00-01.a5qld.mongodb.net:27017,winedb-shard-00-02.a5qld.mongodb.net:27017/${dbName}?ssl=true&replicaSet=atlas-13pr7v-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const uri = `mongodb://ossian:${process.env.SECRET_KEY}@winedb-shard-00-00.a5qld.mongodb.net:27017,winedb-shard-00-01.a5qld.mongodb.net:27017,winedb-shard-00-02.a5qld.mongodb.net:27017/${dbName}?ssl=true&replicaSet=atlas-13pr7v-shard-0&authSource=admin&retryWrites=true&w=majority`;
 const { connection } = mongoose;
 
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true } );
