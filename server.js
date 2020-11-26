70088fcda619f2c1fcef85f8346fbc3c23e22636
OssianN <ossian.northen@gmail.com>
2020-11-26 09:55:49 +0100

    axios

--- server.js
+++ server.js
@@ -5,19 +5,11 @@ const wine = require('./routes');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 const uri = process.env.SECRET_KEY;
 const db = process.env.MONGODB_URL;
 const { connection } = mongoose;
 
-mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true } );
+mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true } );
 
 connection
   .once('open', () => {
@@ -25,11 +17,16 @@ connection
   })
   .on('error', err => console.log('Ooopsiwhoops, some sort of error', err));
 
-const localPort = 5000;
 app.use(cors());
-app.options('*', cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use(express.static('client/build'));
 app.use('/wines', wine);
 
+const localPort = 5000;
 app.listen(process.env.PORT || localPort, () => console.log(`test-api is running on port ${localPort}`));
