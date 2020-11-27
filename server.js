fb10b9fe703a06b0fdf8f5bcd7cf248ca28e7b22
OssianN <ossian.northen@gmail.com>
2020-11-27 09:00:29 +0100

    cors

--- server.js
+++ server.js
@@ -19,9 +19,14 @@ connection
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 
+  'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if( req.method === 'OPTIONS'){
+    req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
   next();
 });
 app.use(express.json());
