6c36155b7fd3a78c7ca968d1c53b147dfc7a42c0
OssianN <ossian.northen@gmail.com>
2021-03-15 20:35:30 +0100

    bug fix for web scraping

--- scraping/cheerio.js
+++ scraping/cheerio.js
@@ -3,15 +3,15 @@ const fetch = require('node-fetch');
 
 const getImage = async title => {
   const searchTitle = title.split(' ').join('+')
+  const cleanSearchTitle = searchTitle.normalize('NFD').replace(/[\u0300-\u036f’]/g, "");
 
-  const vivinoSite = await fetch(`https://www.vivino.com/search/wines?q=${searchTitle}`);
+  const vivinoSite = await fetch(`https://www.vivino.com/search/wines?q=${cleanSearchTitle}`);
   const body = await vivinoSite.text();
   const html = cheerio.load(body);
 
-  const img = html('figure.wine-card__image')[0].attribs.style;
+  const img = html('.wine-card__image')[0].attribs.style;
   const imgURL = img.match(/\/\/.*(?=\))/)
 
-  console.log(searchTitle)
   return imgURL[0];
 }
 
